d533c62ddb7b2d374c94262a597f781c3f807d53
DoDayum <dodayum@163.com>
2022-08-03 22:13:06 +0800

    remove error code

--- service/service/urlService.go
+++ service/service/urlService.go
@@ -3,25 +3,26 @@ package service
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/goccy/go-json"
 	"log"
 	"net/http"
 	"super-blog/models"
 )
 
 func QueryUrl(c *gin.Context) {
+	var err error
 	// x-www-form-urlencoded
 	age := c.PostForm("age")
 	fmt.Printf(age)
 
 	// application/json
-	body, _ := c.GetRawData()
-	var m map[string]interface{}
-	err := json.Unmarshal(body, &m)
-	if err != nil {
-		return
-	}
-	fmt.Printf("%v", m)
+	//body, _ := c.GetRawData()
+	//var m map[string]interface{}
+	//err := json.Unmarshal(body, &m)
+	//if err != nil {
+	//	panic(err.Error())
+	//	return
+	//}
+	//fmt.Printf("%v", m)
 
 	data := make([]*models.Url, 0)
 	err = models.DB.Debug().Find(&data).Error
